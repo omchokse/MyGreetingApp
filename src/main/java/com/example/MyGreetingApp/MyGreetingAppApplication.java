a2f740f92217f47428759de6ba6208b551a6b2d7
Om Chokse <omchokse30@gmail.com>
2025-03-06 10:58:52 +0530

    Added UC1: Basic GreetingController

--- src/main/java/com/example/MyGreetingApp/MyGreetingAppApplication.java
+++ src/main/java/com/example/MyGreetingApp/MyGreetingAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyGreetingAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyGreetingAppApplication.class, args);
 	}
-
 }
